ced57a84432518a16350d18c72085fc0790a1e48
Wolf Mermelstein <wolf@404wolf.com>
2025-01-21 17:56:39 -0500

    three sliders

--- src/lampi/lampi_app.py
+++ src/lampi/lampi_app.py
@@ -1,2 +1,17 @@
-class LampApp(object):
-    pass
+from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.slider import Slider
+
+
+class LampiSlider(Slider):
+    def __init__(self, **kwargs):
+        super(LampiSlider, self).__init__(**kwargs)
+        self.size_hint = (1, 0.33)
+
+
+class LampiApp(App):
+    def __init__(self):
+        super(LampiApp, self).__init__()
+
+    # def build(self):
+    # return Builder.load_file("./lampi/lampi.kv")
